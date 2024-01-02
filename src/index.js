9d601962cc9a78198d4fb4db9cc4d69a3ca92a43
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-01-03 00:42:38 +0530

    path the template

--- src/index.js
+++ src/index.js
@@ -2,34 +2,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 const path = require('path');
-const hbs=require('hbs');
+const hbs = require('hbs');
 
 // Pathing
 const js_path = path.join(__dirname, "../public");
-// const css_path = path.join(__dirname, "../public/css");
-// const img_path=path.join(__dirname,"../public/image");
+const templatePath = path.join(__dirname, "../templates");
+const partialPath = path.join(templatePath, "partials");
 
-app.set('view engine','hbs');
-// app.use(express.static(img_path));
-app.use(express.static(js_path));
-// app.use(express.static(css_path));
-
+app.set('view engine', 'hbs');
+app.set("views", path.join(templatePath, "views"));
+hbs.registerPartials(partialPath);
+app.use(express.static(js_Path));
 
 // Routing
 app.get("/", (req, res) => {
-    res.send("This is Mitanshu's page.");
+    res.render("index");
 });
 
 app.get("/about", (req, res) => {
-    res.send("This is Mitanshu. What should I tell you?");
+    res.render("about");
 });
 
 // Handling 404 - Page Not Found
 app.get("*", (req, res) => {
-    res.send("Oops! This page does not exist.");
+    res.render("404"); // Assuming you have a "404.hbs" view for handling 404 errors
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
- 
